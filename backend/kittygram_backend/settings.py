acdef893e054e7807601f83091fa6780b02c687f
DenisK5 <denis.kondrashov1@gmail.com>
2024-07-19 19:12:17 +0300

    внесены изменения в settings.py в ALLOWED_HOSTS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'token')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
